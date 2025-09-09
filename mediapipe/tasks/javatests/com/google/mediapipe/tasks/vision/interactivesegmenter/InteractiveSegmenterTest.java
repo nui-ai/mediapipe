595c43a1843ad5c85943fe38f6f9a988ecfc404d
matan <matan@nui.ai>
2025-09-09 10:56:30 +0300

    Revert "Allow users to configure NPU delegate"
    This reverts commit 66d4cb7f2f2e517a3d2a6aab62d69bb3195b8abd.

--- mediapipe/tasks/javatests/com/google/mediapipe/tasks/vision/interactivesegmenter/InteractiveSegmenterTest.java
+++ mediapipe/tasks/javatests/com/google/mediapipe/tasks/vision/interactivesegmenter/InteractiveSegmenterTest.java
@@ -17,21 +17,16 @@ package com.google.mediapipe.tasks.vision.interactivesegmenter;
 import static com.google.common.truth.Truth.assertThat;
 
 import android.content.res.AssetManager;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import com.google.mediapipe.framework.image.BitmapExtractor;
 import com.google.mediapipe.framework.image.BitmapImageBuilder;
-import com.google.mediapipe.framework.image.ByteBufferExtractor;
 import com.google.mediapipe.framework.image.MPImage;
 import com.google.mediapipe.tasks.components.containers.NormalizedKeypoint;
 import com.google.mediapipe.tasks.core.BaseOptions;
 import com.google.mediapipe.tasks.vision.imagesegmenter.ImageSegmenterResult;
 import com.google.mediapipe.tasks.vision.interactivesegmenter.InteractiveSegmenter.InteractiveSegmenterOptions;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.Test;
@@ -47,10 +42,8 @@ import org.junit.runners.Suite.SuiteClasses;
 })
 public class InteractiveSegmenterTest {
   private static final String DEEPLAB_MODEL_FILE = "ptm_512_hdt_ptm_woid.tflite";
-  private static final String MAGIC_TOUCH_MODEL_FILE = "magic_touch.tflite";
   private static final String CATS_AND_DOGS_IMAGE = "cats_and_dogs.jpg";
-  private static final String CAT_IMAGE = "cat_large.jpg";
-  private static final float GOLDEN_MASK_SIMILARITY = 0.94f;
+  private static final int MAGNIFICATION_FACTOR = 10;
 
   @RunWith(AndroidJUnit4.class)
   public static final class KeypointRoi extends InteractiveSegmenterTest {
@@ -65,11 +58,11 @@ public class InteractiveSegmenterTest {
               .setOutputConfidenceMasks(false)
               .setOutputCategoryMask(true)
               .build();
-      InteractiveSegmenter interactiveSegmenter =
+      InteractiveSegmenter imageSegmenter =
           InteractiveSegmenter.createFromOptions(
               ApplicationProvider.getApplicationContext(), options);
       MPImage image = getImageFromAsset(inputImageName);
-      ImageSegmenterResult actualResult = interactiveSegmenter.segment(image, roi);
+      ImageSegmenterResult actualResult = imageSegmenter.segment(image, roi);
       assertThat(actualResult.categoryMask().isPresent()).isTrue();
     }
 
@@ -77,44 +70,21 @@ public class InteractiveSegmenterTest {
     public void segment_successWithConfidenceMask() throws Exception {
       final String inputImageName = CATS_AND_DOGS_IMAGE;
       final InteractiveSegmenter.RegionOfInterest roi =
-          InteractiveSegmenter.RegionOfInterest.create(NormalizedKeypoint.create(0.3f, 0.4f));
+          InteractiveSegmenter.RegionOfInterest.create(NormalizedKeypoint.create(0.25f, 0.9f));
       InteractiveSegmenterOptions options =
           InteractiveSegmenterOptions.builder()
               .setBaseOptions(BaseOptions.builder().setModelAssetPath(DEEPLAB_MODEL_FILE).build())
               .setOutputConfidenceMasks(true)
               .setOutputCategoryMask(false)
               .build();
-      InteractiveSegmenter interactiveSegmenter =
+      InteractiveSegmenter imageSegmenter =
           InteractiveSegmenter.createFromOptions(
               ApplicationProvider.getApplicationContext(), options);
       ImageSegmenterResult actualResult =
-          interactiveSegmenter.segment(getImageFromAsset(inputImageName), roi);
+          imageSegmenter.segment(getImageFromAsset(inputImageName), roi);
       assertThat(actualResult.confidenceMasks().isPresent()).isTrue();
       List<MPImage> confidenceMasks = actualResult.confidenceMasks().get();
       assertThat(confidenceMasks.size()).isEqualTo(2);
-    }
-
-    @Test
-    public void segment_successWithMagicTouch() throws Exception {
-      final String inputImageName = CAT_IMAGE;
-      final InteractiveSegmenter.RegionOfInterest roi =
-          InteractiveSegmenter.RegionOfInterest.create(NormalizedKeypoint.create(0.30f, 0.4f));
-      InteractiveSegmenterOptions options =
-          InteractiveSegmenterOptions.builder()
-              .setBaseOptions(
-                  BaseOptions.builder().setModelAssetPath(MAGIC_TOUCH_MODEL_FILE).build())
-              .setOutputConfidenceMasks(false)
-              .setOutputCategoryMask(true)
-              .build();
-      InteractiveSegmenter interactiveSegmenter =
-          InteractiveSegmenter.createFromOptions(
-              ApplicationProvider.getApplicationContext(), options);
-      ImageSegmenterResult actualResult =
-          interactiveSegmenter.segment(getImageFromAsset(inputImageName), roi);
-      assertThat(actualResult.categoryMask()).isPresent();
-      MPImage actualMaskBuffer = actualResult.categoryMask().get();
-      MPImage expectedMaskBuffer = getImageFromAsset("cat_large_mask.png");
-      verifyCategoryMask(actualMaskBuffer, expectedMaskBuffer, GOLDEN_MASK_SIMILARITY);
     }
   }
 
@@ -135,11 +105,11 @@ public class InteractiveSegmenterTest {
               .setOutputConfidenceMasks(false)
               .setOutputCategoryMask(true)
               .build();
-      InteractiveSegmenter interactiveSegmenter =
+      InteractiveSegmenter imageSegmenter =
           InteractiveSegmenter.createFromOptions(
               ApplicationProvider.getApplicationContext(), options);
       MPImage image = getImageFromAsset(inputImageName);
-      ImageSegmenterResult actualResult = interactiveSegmenter.segment(image, roi);
+      ImageSegmenterResult actualResult = imageSegmenter.segment(image, roi);
       assertThat(actualResult.categoryMask().isPresent()).isTrue();
     }
 
@@ -158,36 +128,15 @@ public class InteractiveSegmenterTest {
               .setOutputConfidenceMasks(true)
               .setOutputCategoryMask(false)
               .build();
-      InteractiveSegmenter interactiveSegmenter =
+      InteractiveSegmenter imageSegmenter =
           InteractiveSegmenter.createFromOptions(
               ApplicationProvider.getApplicationContext(), options);
       ImageSegmenterResult actualResult =
-          interactiveSegmenter.segment(getImageFromAsset(inputImageName), roi);
+          imageSegmenter.segment(getImageFromAsset(inputImageName), roi);
       assertThat(actualResult.confidenceMasks().isPresent()).isTrue();
       List<MPImage> confidenceMasks = actualResult.confidenceMasks().get();
       assertThat(confidenceMasks.size()).isEqualTo(2);
     }
-  }
-
-  private static void verifyCategoryMask(
-      MPImage actualMask, MPImage goldenMask, float similarityThreshold) {
-    assertThat(actualMask.getWidth()).isEqualTo(goldenMask.getWidth());
-    assertThat(actualMask.getHeight()).isEqualTo(goldenMask.getHeight());
-    ByteBuffer actualMaskBuffer = ByteBufferExtractor.extract(actualMask);
-    Bitmap goldenMaskBitmap = BitmapExtractor.extract(goldenMask);
-    int consistentPixels = 0;
-    final int numPixels = actualMask.getWidth() * actualMask.getHeight();
-    actualMaskBuffer.rewind();
-    for (int y = 0; y < actualMask.getHeight(); y++) {
-      for (int x = 0; x < actualMask.getWidth(); x++) {
-        boolean actualForground = actualMaskBuffer.get() != 0;
-        boolean goldenForeground = goldenMaskBitmap.getPixel(x, y) != Color.BLACK;
-        if (actualForground == goldenForeground) {
-          ++consistentPixels;
-        }
-      }
-    }
-    assertThat((float) consistentPixels / numPixels).isGreaterThan(similarityThreshold);
   }
 
   private static MPImage getImageFromAsset(String filePath) throws Exception {
