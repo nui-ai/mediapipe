4fdc114521efd41a29c428a764351b07fb89f6ae
matan <matan@nui.ai>
2025-09-09 10:56:23 +0300

    Revert "Migrate HandednessToMatrixCalculator to API3."
    This reverts commit faa62e2c688e9fef8e695321b35ee681aace2efe.

--- mediapipe/tasks/cc/vision/gesture_recognizer/calculators/handedness_to_matrix_calculator_test.cc
+++ mediapipe/tasks/cc/vision/gesture_recognizer/calculators/handedness_to_matrix_calculator_test.cc
@@ -1,4 +1,4 @@
-/* Copyright 2025 The MediaPipe Authors.
+/* Copyright 2022 The MediaPipe Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,26 +12,27 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
-#include "mediapipe/tasks/cc/vision/gesture_recognizer/calculators/handedness_to_matrix_calculator.h"
 
+#include <memory>
 #include <string>
+#include <utility>
+#include <vector>
 
-#include "mediapipe/framework/api3/function_runner.h"
-#include "mediapipe/framework/api3/graph.h"
-#include "mediapipe/framework/api3/packet.h"
-#include "mediapipe/framework/api3/stream.h"
+#include "absl/memory/memory.h"
+#include "mediapipe/framework/calculator_framework.h"
+#include "mediapipe/framework/calculator_runner.h"
 #include "mediapipe/framework/formats/classification.pb.h"
 #include "mediapipe/framework/formats/matrix.h"
 #include "mediapipe/framework/port/gtest.h"
+#include "mediapipe/framework/port/parse_text_proto.h"
 #include "mediapipe/framework/port/status_matchers.h"
 
 namespace mediapipe {
+
 namespace {
 
-using ::mediapipe::api3::GenericGraph;
-using ::mediapipe::api3::Packet;
-using ::mediapipe::api3::Runner;
-using ::mediapipe::api3::Stream;
+constexpr char kHandednessTag[] = "HANDEDNESS";
+constexpr char kHandednessMatrixTag[] = "HANDEDNESS_MATRIX";
 
 mediapipe::ClassificationList ClassificationForHandedness(float handedness) {
   mediapipe::ClassificationList result;
@@ -57,26 +58,24 @@ using HandednessToMatrixCalculatorTest =
 TEST_P(HandednessToMatrixCalculatorTest, OutputsCorrectResult) {
   const HandednessToMatrixCalculatorTestCase& test_case = GetParam();
 
-  MP_ASSERT_OK_AND_ASSIGN(
-      auto runner,
-      Runner::For([&](GenericGraph& graph,
-                      Stream<mediapipe::ClassificationList> handedness)
-                      -> Stream<Matrix> {
-        auto& node = graph.AddNode<tasks::HandednessToMatrixNode>();
-        node.in_handedness.Set(handedness);
-        return node.out_handedness_matrix.Get();
-      }).Create());
+  auto node_config = ParseTextProtoOrDie<CalculatorGraphConfig::Node>(
+      R"pb(
+        calculator: "HandednessToMatrixCalculator"
+        input_stream: "HANDEDNESS:handedness"
+        output_stream: "HANDEDNESS_MATRIX:handedness_matrix"
+      )pb");
+  CalculatorRunner runner(node_config);
 
-  mediapipe::ClassificationList input_handedness =
-      ClassificationForHandedness(test_case.handedness);
+  auto input_handedness = std::make_unique<mediapipe::ClassificationList>();
+  *input_handedness = ClassificationForHandedness(test_case.handedness);
+  runner.MutableInputs()
+      ->Tag(kHandednessTag)
+      .packets.push_back(Adopt(input_handedness.release()).At(Timestamp(0)));
 
-  MP_ASSERT_OK_AND_ASSIGN(
-      Packet<Matrix> output_packet,
-      runner.Run(
-          api3::MakePacket<mediapipe::ClassificationList>(input_handedness)));
+  MP_ASSERT_OK(runner.Run()) << "Calculator execution failed.";
 
-  ASSERT_TRUE(output_packet);
-  const Matrix& handedness = output_packet.GetOrDie();
+  const auto handedness =
+      runner.Outputs().Tag(kHandednessMatrixTag).packets[0].Get<Matrix>();
   ASSERT_EQ(1, handedness.cols());
   ASSERT_EQ(1, handedness.rows());
   EXPECT_NEAR(handedness(0, 0), test_case.handedness, .001f);
@@ -92,10 +91,6 @@ INSTANTIATE_TEST_CASE_P(
       return info.param.test_name;
     });
 
-TEST(HandednessToMatrixCalculatorTest, HasCorrectRegistrationName) {
-  EXPECT_EQ(tasks::HandednessToMatrixNode::GetRegistrationName(),
-            "HandednessToMatrixCalculator");
-}
-
 }  // namespace
+
 }  // namespace mediapipe
