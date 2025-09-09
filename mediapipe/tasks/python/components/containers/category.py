eb6434b3eb85bb765144fb4d0e1c3476045a5dd2
matan <matan@nui.ai>
2025-09-09 10:56:29 +0300

    Revert "Internal changes to Python package"
    This reverts commit 46be2cde74c575ad3f4d156e8c90f2c31b6aa608.

--- mediapipe/tasks/python/components/containers/category.py
+++ mediapipe/tasks/python/components/containers/category.py
@@ -51,8 +51,7 @@ class Category:
         index=self.index,
         score=self.score,
         label=self.category_name,
-        display_name=self.display_name if self.display_name else None,
-    )
+        display_name=self.display_name)
 
   @classmethod
   @doc_controls.do_not_generate_docs
