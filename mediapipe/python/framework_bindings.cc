de0bc2ec2c10a1539154977b60152ba7e3e98854
matan <matan@nui.ai>
2025-10-02 12:15:11 +0300

    revert back files removed to not to reference non-hands build targets and sources whose source have been previously trimmed away to focus development work by not showing up such sources on symbol searches. needed for the python bindings build.

--- mediapipe/python/framework_bindings.cc
+++ mediapipe/python/framework_bindings.cc
@@ -23,7 +23,6 @@
 #include "mediapipe/python/pybind/resource_util.h"
 #include "mediapipe/python/pybind/timestamp.h"
 #include "mediapipe/python/pybind/validated_graph_config.h"
-#include "mediapipe/tasks/python/core/pybind/task_runner.h"
 
 namespace mediapipe {
 namespace python {
@@ -43,7 +42,7 @@ PYBIND11_MODULE(_framework_bindings, m) {
   // As all MediaPipe calculators and Python bindings need to go into a single
   // .so file, having MediaPipe Tasks' task runner module in _framework_bindings
   // as well.
-  tasks::python::TaskRunnerSubmodule(&m);
+  // tasks::python::TaskRunnerSubmodule(&m);
 }
 
 }  // namespace python
