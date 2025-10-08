a22080f20a118a593d4d7eacf3802724c7d9844a
matan <matan@nui.ai>
2025-10-08 20:02:17 +0300

    attempt to use static linking, which will be immediately rolled back, see https://github.com/nui-ai/mediapipe/issues/22

--- rust/build.rs
+++ rust/build.rs
@@ -40,15 +40,7 @@ fn main() {
     //    can find the .so file.
 
     let so_dir = format!("{}/../bazel-bin/mediapipe/examples/desktop/hand_tracking", env!("CARGO_MANIFEST_DIR"));
-    // direct rustc to find the lib file (.so) during its compilation and linking when building the rust binary
-    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", so_dir);
-    // make the runtime find the same lib file when running that rust built binary
+    // direct rustc to find the static lib file (.a) during its compilation and linking when building the rust binary
     println!("cargo:rustc-link-search=native={}", so_dir);
-    // static linking would trigger a cascade of rendering all dependencies of the C API as static libraries,
-    // which is typically intractable or extremely hard to accoplish, so dynamic linking is our only option.
-    // we could also directly feed in an object file from the Bazel build, not a lib built by it, but then we also
-    // have to cater for aspects that linking on the bazel side took ownership of before, not to mention that bazel
-    // does not in its default behavior leave behind object files built for cc_library rules for cargo to consume.
-    // the object of integration between C and rust (pun not intended) is the library file.
-    println!("cargo:rustc-link-lib=dylib=hands_pipeline_operator_c_api");
+    println!("cargo:rustc-link-lib=static=hands_pipeline_operator_c_api_static");
 }
